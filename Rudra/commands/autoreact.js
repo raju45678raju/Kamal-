0b1065d2f59cf176da182659666d2d1061312202
SANANKHAN302 <143243818+SANANKHAN302@users.noreply.github.com>
2025-08-17 00:03:40 +0500

    Update autoreact.js

--- Rudra/commands/autoreact.js
+++ Rudra/commands/autoreact.js
@@ -25,7 +25,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         body: ""
       }
       api.sendMessage(lab, threadID, messageID);
-    api.setMessageReaction("❤️", event.messageID, (err) => {}, true)
+    api.setMessageReaction("♥️", event.messageID, (err) => {}, true)
           };
     if(react.includes("sakit") || react.includes("Sakit") || react.includes("saket") || react.includes("Saket") || react.includes("peyn") || react.includes("Peyn") || react.includes("Pain") || react.includes("mamatay") || react.includes("Mamatay") || react.includes("ayaw ko na") || react.includes("Ayaw ko na") || react.includes("saktan") || react.includes("Saktan") || react.includes("Sasaktan") || react.includes("sasaktan") || react.includes("sad") || react.includes("Sad") || react.includes("malungkot") || react.includes("Malungkot") || react.includes(" 😥") || react.includes("😰") || react.includes("😨") || react.includes("😢") || react.includes(":(") || react.includes("😔") || react.includes("😞") || react.includes("depress") || react.includes("stress") || react.includes("Stress") || react.includes("Depress") || react.includes("depression") || react.includes("Depression") || react.includes("kalungkutan") || react.includes("Kalungkutan") || react.includes("😭")) {
       var sad = {
@@ -39,7 +39,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         body: ""
       }
       api.sendMessage(flag, threadID, messageID);
-    api.setMessageReaction("🇮🇳", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🇦🇫", event.messageID, (err) => {}, true)
           };
   if(react.includes("eve") || react.includes("Eve") || react.includes("morning") || react.includes("Morning") || react.includes("afternoon") || react.includes("Afternoon") || react.includes("evening") || react.includes("eat") || react.includes("Eat") || react.includes("night") || react.includes("nyt") || react.includes("Night") || react.includes("Nyt")) {
       var heart = {
